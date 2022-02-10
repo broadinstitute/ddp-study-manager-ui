649ccb4e9161c88e30441dbbe420a5d0e68bb52c
gmakhara <gugamaxara@github.com>
2022-02-10 17:04:02 +0400

    DDP-7465 | add sort model class

---
+++ src/app/sort/sort.model.spec.ts
@@ -0,0 +1,7 @@
+import { Sort } from './sort.model';
+
+describe('Sort', () => {
+  it('should create an instance', () => {
+    expect(new Sort()).toBeTruthy();
+  });
+});
---
+++ src/app/sort/sort.model.ts
@@ -0,0 +1,20 @@
+import { Filter } from "../filter-column/filter-column.model";
+
+export class Sort {
+
+    constructor(public type: string, public additionalType: string, public tableAlias: string,
+        public outerProperty: string, public innerProperty: string, public order: string) {
+            this.type = type;
+            this.additionalType = additionalType;
+            this.tableAlias = tableAlias;
+            this.outerProperty = outerProperty;
+            this.innerProperty = innerProperty;
+            this.order = order;
+    }
+
+    static parse(filter: Filter, order: string): Sort {
+        return new Sort(filter.type, filter.additionalType, filter.participantColumn.tableAlias, 
+            filter.participantColumn.object, filter.participantColumn.name, order);
+    }
+
+}
